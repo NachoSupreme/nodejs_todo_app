c0cfd0d0d916c216933a87357178e740c204c6ee
Majed <majedchaaban@gmail.com>
2017-09-13 20:02:28 -0400

    add mongodb update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,27 @@
+const {MongoClient, ObjectID} = require("mongodb");
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp",(err, db) => {
+  if(err) {
+    return console.log(`Unable to connect to MongoDB server ${err}`);
+  }
+  console.log(`Connected to Mongodb server`);
+  
+  //findOneAndUpdate
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID("59827d6b4a4de9f018277166")
+  },{
+    $set: {
+      name: "Charlie 'Alexander the Great' Chaaban"
+    },
+    $inc: {
+      age: -13
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+ 
+  //db.close();
+});
