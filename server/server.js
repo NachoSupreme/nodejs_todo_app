41365ecbc728ca511bd8e463cda4246d681b6882
Majed <majedchaaban@gmail.com>
2017-09-16 13:00:15 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -27,8 +27,12 @@ app.post("/todos", (req, res)=> {
 
 
 //Read route
-app.get("/", (req, res)=> {
-
+app.get("/todos", (req, res)=> {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
 });
 
 
