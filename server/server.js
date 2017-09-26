54df433fa9177431d3dd38d71786cca8d84202a0
Majed <majedchaaban@gmail.com>
2017-09-25 21:16:08 -0400

    Setup seperate test and development environments

--- server/server.js
+++ server/server.js
@@ -1,3 +1,15 @@
+var env = process.env.NODE_ENV || "development";
+console.log(`env *******`, env);
+
+if (env === "development") {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = "mongodb://localhost:27017/ToDoApp"
+} else if (env === "test") {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = "mongodb://localhost:27017/ToDoAppTest"
+}
+
+
 //server.js file is only responsible for routes
 const _ = require("lodash");
 const express = require("express");
